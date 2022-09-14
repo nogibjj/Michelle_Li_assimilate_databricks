cd7c622970fbda85da3e505629482524bd48f0ba
Michelle Li <70456530+michellejieli@users.noreply.github.com>
2022-09-14 18:57:33 +0000

    update enron file to replace with ds_salaries dataset

--- dblib/enron.py
+++ dblib/enron.py
@@ -11,14 +11,14 @@ import pandas as pd
 import dask.dataframe as dd
 
 
-def pandas_load_enron(location="datasets/emails.csv"):
+def pandas_load_enron(location="datasets/ds_salaries.csv"):
     """Load the enron dataset into a pandas dataframe
     
     Assumes the dataset is in the datasets folder in the root of the project
     """
     return pd.read_csv(location)
 
-def pandas_print_enron(location="datasets/emails.csv", record_number=0):
+def pandas_print_enron(location="datasets/ds_salaries.csv", record_number=0):
     """Display the enron dataset
     
     print and returns a single email record from the enron dataset
@@ -29,7 +29,7 @@ def pandas_print_enron(location="datasets/emails.csv", record_number=0):
     print(record)
     return record
 
-def dask_query_enron(location="datasets/emails.csv"):
+def dask_query_enron(location="datasets/ds_salaries.csv"):
     """Query the enron dataset
     
     Assumes the dataset is in the datasets folder in the root of the project
